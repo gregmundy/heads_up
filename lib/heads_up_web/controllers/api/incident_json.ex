c6368e96e059d8bea853e088ea2c1875d7ebefb2
Greg Mundy <gregmundy@gmail.com>
2025-05-03 09:50:05 -0400

    feat: implement incidents api

---
+++ lib/heads_up_web/controllers/api/incident_json.ex
@@ -0,0 +1,29 @@
+defmodule HeadsUpWeb.Api.IncidentJSON do
+  def index(%{incidents: incidents}) do
+    %{
+      incidents: [
+        for incident <- incidents do
+          data(incident)
+        end
+      ]
+    }
+  end
+
+  def show(%{incident: incident}) do
+    %{
+      incident: data(incident)
+    }
+  end
+
+  defp data(incident) do
+    %{
+      id: incident.id,
+      name: incident.name,
+      priority: incident.priority,
+      status: incident.status,
+      description: incident.description,
+      image_path: incident.image_path,
+      category_id: incident.category_id
+    }
+  end
+end
