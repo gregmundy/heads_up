ab750b9eebcd517f8fb7aadeaabda266edcf19a1
Greg Mundy <gregmundy@gmail.com>
2025-05-04 00:45:28 -0400

    feat: add custom username

--- lib/heads_up/accounts/user.ex
+++ lib/heads_up/accounts/user.ex
@@ -4,6 +4,7 @@ defmodule HeadsUp.Accounts.User do
 
   schema "users" do
     field :email, :string
+    field :username, :string
     field :password, :string, virtual: true, redact: true
     field :hashed_password, :string, redact: true
     field :current_password, :string, virtual: true, redact: true
@@ -37,9 +38,10 @@ defmodule HeadsUp.Accounts.User do
   """
   def registration_changeset(user, attrs, opts \\ []) do
     user
-    |> cast(attrs, [:email, :password])
+    |> cast(attrs, [:email, :password, :username])
     |> validate_email(opts)
     |> validate_password(opts)
+    |> validate_username()
   end
 
   defp validate_email(changeset, opts) do
@@ -61,6 +63,15 @@ defmodule HeadsUp.Accounts.User do
     # |> validate_format(:password, ~r/[A-Z]/, message: "at least one upper case character")
     # |> validate_format(:password, ~r/[!?@#$%^&*_0-9]/, message: "at least one digit or punctuation character")
     |> maybe_hash_password(opts)
+  end
+
+  defp validate_username(changeset) do
+    changeset
+    |> validate_required([:username])
+    |> validate_length(:username, min: 2, max: 25)
+    |> validate_format(:username, ~r/^[a-zA-Z0-9_-]+$/, message: "must contain only lowercase letters and numbers")
+    |> unsafe_validate_unique(:username, HeadsUp.Repo)
+    |> unique_constraint(:username)
   end
 
   defp maybe_hash_password(changeset, opts) do
